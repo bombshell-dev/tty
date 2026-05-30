19631a2f7319acb6353ae524acc313e7dc13ac37
Nate Moore <git@natemoo.re>
2026-05-30 04:54:36 -0500

    ref(bench): fix return

--- bench/startup.bench.ts
+++ bench/startup.bench.ts
@@ -5,8 +5,12 @@ import { spawnFixture } from "./fixtures/utils.ts";
 let bench = withCodSpeed(new Bench({ name: "startup" }));
 
 bench
-  .add("createTerm", () => spawnFixture("create-term"))
-  .add("time to first render", () => spawnFixture("render-minimal"));
+  .add("createTerm", () => {
+    spawnFixture("create-term");
+  })
+  .add("time to first render", () => {
+    spawnFixture("render-minimal");
+  });
 
 await bench.run();
 console.table(bench.table());
