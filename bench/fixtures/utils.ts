baa6193211187305bf2e030259c5b30cbca39532
Nate Moore <git@natemoo.re>
2026-05-30 04:35:22 -0500

    ref(bench): spawn fixture

---
+++ bench/fixtures/utils.ts
@@ -0,0 +1,9 @@
+import { fileURLToPath } from "node:url";
+import { spawnSync } from "node:child_process";
+
+export const fixture = (name: string) => {
+    return new URL(`./${name}/mod.ts`, import.meta.url);
+}
+
+export const spawnFixture = (name: string) =>
+  spawnSync(process.execPath, [...process.execArgv, fileURLToPath(fixture(name))], { stdio: "ignore" });
